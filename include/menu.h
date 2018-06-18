d03e803cc66944c86f5f84a139449dcfe2dfb967
Vanderlei Jr <vanderlei.brito.junior@gmail.com>
2018-06-18 19:24:40 -0300

    Criada uma função para intro

--- include/menu.h
+++ include/menu.h
@@ -2,6 +2,7 @@
 #define MENU_H
 
 void Initializers();
+void Intro(ALLEGRO_DISPLAY *display, ALLEGRO_BITMAP *intro, int sprites);
 void FirstMenu();
 
 #endif //MENU_H
--- src/menu.c
+++ src/menu.c
@@ -11,7 +11,38 @@ void Initializers(){
 	al_install_keyboard();
 }
 
+void Intro(ALLEGRO_DISPLAY *display, ALLEGRO_BITMAP *intro, int sprites){
+	int currentSprite, introWidth, introHeight, alpha;
+
+	introWidth = al_get_bitmap_width(intro) / sprites;
+	introHeight = al_get_bitmap_height(intro);
+
+	for(currentSprite = 0; currentSprite <= sprites - 1; currentSprite++){
+		if(!currentSprite){
+			for(alpha = 0; alpha <= 195; alpha += 15){
+				al_draw_tinted_bitmap_region(intro, al_map_rgba(alpha, alpha, alpha, alpha), (currentSprite % sprites) * introWidth, 0, introWidth, introHeight, WIDTH * 0.5 - introWidth * 0.5, HEIGHT * 0.5 - introHeight * 0.5, 0);
+				al_flip_display();
+				al_rest(0.1);
+			}
+		}
+
+		al_draw_bitmap_region(intro, (currentSprite % sprites) * introWidth, 0, introWidth, introHeight, WIDTH * 0.5 - introWidth * 0.5, HEIGHT * 0.5 - introHeight * 0.5, 0);
+		al_flip_display();
+		al_rest(0.25);
+
+		if(currentSprite == sprites - 1){
+			for(alpha = 195; alpha >= 0; alpha -= 15){
+				al_clear_to_color(COLOR_BLACK);
+				al_draw_tinted_bitmap_region(intro, al_map_rgba(alpha, alpha, alpha, alpha), (currentSprite % sprites) * introWidth, 0, introWidth, introHeight, WIDTH * 0.5 - introWidth * 0.5, HEIGHT * 0.5 - introHeight * 0.5, 0);
+				al_flip_display();
+				al_rest(0.1);
+			}
+		}
+	}
+}
+
 void FirstMenu(){
+
 	//Variaveis
 	bool exit = false;
 	ALLEGRO_EVENT event;
@@ -20,8 +51,9 @@ void FirstMenu(){
 	ALLEGRO_TIMER *timer = al_create_timer(1.0 / FPS);
 	ALLEGRO_DISPLAY *display = al_create_display(WIDTH, HEIGHT);
 	ALLEGRO_EVENT_QUEUE *eventQueue = al_create_event_queue();
+	ALLEGRO_BITMAP *introOne = al_load_bitmap("../media/img/intro_one.png");
 
-
+	//Configurações Internas
 	al_set_window_title(display, GAME_TITLE);
 	al_register_event_source(eventQueue, al_get_keyboard_event_source());
 	al_register_event_source(eventQueue, al_get_display_event_source(display));
@@ -29,8 +61,10 @@ void FirstMenu(){
 
 	al_start_timer(timer);
 
+	Intro(display, introOne, 12);
+
 	while(!exit){
-		al_flip_display();
+		//al_flip_display();
 		al_wait_for_event(eventQueue, &event);
 
 		if(event.type == ALLEGRO_EVENT_KEY_DOWN){

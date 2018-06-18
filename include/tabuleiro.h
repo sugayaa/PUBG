37f2be16817fe2c22ecb51ee241cb7e57facdef9
vitoor-s <vitorsugaya5@gmail.com>
2018-06-18 01:03:07 -0300

    Adicionado módulo do tabuleiro e arquivo de constantes

---
+++ include/tabuleiro.h
@@ -0,0 +1,23 @@
+#ifndef TABULEIRO_H
+#define TABULEIRO_H
+#include "constants.h"
+
+//fila estaticamente implementada
+typedef struct tabTile{
+	int nJogadores;
+	char efeito;
+}tabTile;
+
+typedef struct tabuleiro{
+	tabTile corpo[TAM];
+	int inicio;
+	int _final;
+}tabuleiro;
+
+void Limpa(tabuleiro*);
+void RandomizaTabuleiro(tabuleiro*);
+void Randomiza(tabuleiro*, int, int);
+void Inicia(tabuleiro*);
+void Armageddon(tabuleiro*);
+
+#endif
---
+++ src/tabuleiro.c
@@ -0,0 +1,53 @@
+#include"../include/tabuleiro.h"
+#include<time.h>
+
+void Limpa(tabuleiro* t){
+	int i;
+	for(i = 0; i < TAM; i++){
+		t->corpo[i].nJogadores = 0;
+		t->corpo[i].efeito = 0;
+	}
+}
+
+void RandomizaTabuleiro(tabuleiro* t){
+	int i;
+	srand(time(NULL));
+	for(i = 0; i < TAM; i++){
+		if(rand()%3 == 0){
+			//t->corpo[i].efeito = 65 + (rand()%3);
+		t->corpo[i].efeito = 1;
+		}
+	}
+}
+
+void Randomiza(tabuleiro* t, int daOnde, int ateOnde){
+	int i;
+	srand(time(NULL));
+	for(i = daOnde; i != ateOnde + 1; i = (i+1)%TAM){
+		if(rand()%3 == 0){
+			//t->corpo[i].efeito = 65 + (rand()%3);
+		t->corpo[i].efeito = 1;
+		}
+	}
+}
+
+void Inicia(tabuleiro* t){
+	Limpa(t);
+	RandomizaTabuleiro(t);
+}
+
+void Armageddon(tabuleiro* t){
+	srand(time(NULL));
+	int intensidade = (rand()%5) + 1;
+
+	//verificar a partir dos players e n de todos as peças do tabuleiro
+	//for i in players
+	//if(player in inicio + intensidade
+	//player dead
+	
+	t->_final = (t->_final+ intensidade) % 100;
+}
+
+int main(){
+	return 0;
+}

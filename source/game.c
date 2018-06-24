5b529fd5466b264e6a2c6aa8fbfbc401e9f3ff4c
Vanderlei Jr <vanderlei.brito.junior@gmail.com>
2018-06-24 15:31:51 -0300

    Correção das posições

--- source/game.c
+++ source/game.c
@@ -33,12 +33,12 @@ void Game(ALLEGRO_DISPLAY *display, ALLEGRO_TIMER *timer,
 
 		switch(numberPlayers){
 			case 4:
-				al_draw_bitmap_region(spritePlayers, 3 * 128, 0, 128, 128, playersPositions[getPosicao(&players[3])].x + 75, playersPositions[getPosicao(&players[3])].y + 30, 0);
+				al_draw_bitmap_region(spritePlayers, 3 * 128, 0, 128, 128, playersPositions[getPosicao(&players[3])].x, playersPositions[getPosicao(&players[3])].y, 0);
 			case 3:
-				al_draw_bitmap_region(spritePlayers, 2 * 128, 0, 128, 128, playersPositions[getPosicao(&players[2])].x + 25, playersPositions[getPosicao(&players[2])].y + 30, 0);
+				al_draw_bitmap_region(spritePlayers, 2 * 128, 0, 128, 128, playersPositions[getPosicao(&players[2])].x + 50, playersPositions[getPosicao(&players[2])].y, 0);
 			case 2:
-				al_draw_bitmap_region(spritePlayers, 0 * 128, 0, 128, 128, playersPositions[getPosicao(&players[0])].x, playersPositions[getPosicao(&players[0])].y, 0);
-				al_draw_bitmap_region(spritePlayers, 1 * 128, 0, 128, 128, playersPositions[getPosicao(&players[1])].x + 50, playersPositions[getPosicao(&players[1])].y, 0);
+				al_draw_bitmap_region(spritePlayers, 1 * 128, 0, 128, 128, playersPositions[getPosicao(&players[1])].x + 25, playersPositions[getPosicao(&players[1])].y + 30, 0);
+				al_draw_bitmap_region(spritePlayers, 0 * 128, 0, 128, 128, playersPositions[getPosicao(&players[0])].x + 75, playersPositions[getPosicao(&players[0])].y + 30, 0);
 				break;
 		}
 

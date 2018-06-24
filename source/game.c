f34eac3e41348487f6c01715a3d4bf30fcafa5f3
Vanderlei Jr <vanderlei.brito.junior@gmail.com>
2018-06-24 15:21:04 -0300

    Configuração inicial das posições do mapa

--- source/game.c
+++ source/game.c
@@ -31,13 +31,16 @@ void Game(ALLEGRO_DISPLAY *display, ALLEGRO_TIMER *timer,
 		al_clear_to_color(COLOR_BLACK);
 		al_draw_bitmap(gameBackground, 0, 0, 0);
 
-		int i;
-		for(i = 0; i < numberPlayers; i++)
-			al_draw_bitmap_region(spritePlayers, players[i].ID * 128, 0, 128, 128, getPosicao(&players[i]) + players[i].ID * 30, getPosicao(&players[i]) + 30, 0);
-		//
-		printf("Vez do jogador %d !\nRole sua sorte\n", front(&playersQueue).ID+1);
-		//
-
+		switch(numberPlayers){
+			case 4:
+				al_draw_bitmap_region(spritePlayers, 3 * 128, 0, 128, 128, playersPositions[getPosicao(&players[3])].x + 75, playersPositions[getPosicao(&players[3])].y + 30, 0);
+			case 3:
+				al_draw_bitmap_region(spritePlayers, 2 * 128, 0, 128, 128, playersPositions[getPosicao(&players[2])].x + 25, playersPositions[getPosicao(&players[2])].y + 30, 0);
+			case 2:
+				al_draw_bitmap_region(spritePlayers, 0 * 128, 0, 128, 128, playersPositions[getPosicao(&players[0])].x, playersPositions[getPosicao(&players[0])].y, 0);
+				al_draw_bitmap_region(spritePlayers, 1 * 128, 0, 128, 128, playersPositions[getPosicao(&players[1])].x + 50, playersPositions[getPosicao(&players[1])].y, 0);
+				break;
+		}
 
 		al_flip_display();
 		al_wait_for_event(eventQueue, event);
@@ -49,8 +52,6 @@ void Game(ALLEGRO_DISPLAY *display, ALLEGRO_TIMER *timer,
 			}
 
 			if(event->keyboard.keycode == ALLEGRO_KEY_SPACE){
-				players[0].pos++;
-
 				int avanco = (rand() % TAM_DADO) + 1;
 
 				//push(&playersQueue, pop(&playersQueue));
@@ -59,7 +60,6 @@ void Game(ALLEGRO_DISPLAY *display, ALLEGRO_TIMER *timer,
 				if(players[atual.ID].pos >= TAM)
 					printf("Jogador %d ganhou o jogo!!", atual.ID + 1);
 				push(&playersQueue, players[atual.ID]);
-
 			}
 		}
 		//Clicar no X para SAIR
